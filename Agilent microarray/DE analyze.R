daa1544c2d6fd3a55b03e6e7fb3618f39121c069
Di Wu <101570925+Pancreatologist@users.noreply.github.com>
2023-07-13 17:18:31 +0100

    Update DE analyze.R

--- Agilent microarray/DE analyze.R
+++ Agilent microarray/DE analyze.R
@@ -68,4 +68,69 @@ summary(degs_temp)
 degs_temp$logP <- -log10(degs_temp$adj.P.Val)
 write.csv(degs_temp[c('GeneName','Description','ENTREZID','logFC','AveExpr','t','P.Value','B','Condition')],'DEG.csv')#export each DE results for the Table S1
 
+### visualization for DE
 
+volcanoplot<-ggscatter(degs_temp,x = "logFC", y = "logP",color = "Condition",palette = c("#CB9B8F", "gray" ,"#5B7493"  ),
+                size=2,
+                xlab = "Log2FC",
+                ylab = "-Log10(Adj.P-value)",)+ 
+  ggtitle(paste("Volcano plot for \n", contrastgroup))+
+  ylim(0,15)+ 
+  xlim(-3,3)+ 
+  geom_hline(yintercept = 1.3 , linetype ="dashed")+ 
+  geom_vline(xintercept = c(-1,1), linetype= "dashed")+
+  labs(subtitle = paste(sprintf("up: %d", table(degs_temp$Condition)["Up"]),  sprintf("down: %d", table(degs_temp$Condition)["Down"]), sep = " | "))+
+  theme(legend.position = "right")+
+  theme_cowplot(20)
+
+### upset plot for DE
+# remove genes not signif. in any contrast
+results <- results[rowSums(abs(results)) > 0, ]
+# calculate the intersection between the differentially expressed gene sets
+m <- make_comb_mat(abs(results), mode = "distinct")
+# exclude self-intersects (total # of diff. genes will be displayed separately)
+m <- m[comb_degree(m) > 1]
+(ss <- set_size(m))
+(cs <- comb_size(m))
+ht <- UpSet(m, 
+            set_order = colnames(m), comb_col = "#f9d099",
+            comb_order = order(comb_degree(m)),
+            top_annotation = HeatmapAnnotation(
+              "Distinct diff. genes" = anno_barplot(
+                cs, 
+                ylim = c(0, max(cs)*1.1),
+                border = FALSE, 
+                gp = gpar(fill = "black"), 
+                height = unit(4, "cm")
+              ), 
+              annotation_name_side = "left", 
+              annotation_name_rot = 90),
+            right_annotation = HeatmapAnnotation(
+              which = "row",
+              "Total" = anno_barplot(
+                ss, 
+                ylim = c(0, max(ss)*1.1),
+                border = FALSE, 
+                gp = gpar(fill = "black"), 
+                width = unit(4, "cm")
+              )
+            ),
+            column_title = "Intersection between contrasts"
+)
+ht = draw(ht)
+od = column_order(ht)
+rod = row_order(ht)
+decorate_annotation("Distinct diff. genes", {
+  grid.text(cs[od], 
+            x = seq_along(cs), 
+            y = unit(cs[od], "native") + unit(2, "pt"), 
+            default.units = "native", just = c("center", "bottom"), 
+            gp = gpar(fontsize = 8, col = "#404040"))
+})
+decorate_annotation("Total", {
+  grid.text(ss[rod], 
+            x = unit(ss[rod], "native") + unit(20, "pt"), 
+            y = rev(seq_along(ss)), 
+            default.units = "native", just = c("right", "bottom"), 
+            gp = gpar(fontsize = 8, col = "#404040"))
+})
